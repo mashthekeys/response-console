ec4f91141ee21f1147ccaab4a4669de99f735126
Andy Holland <github@ahweb.co.uk>
2021-09-18 01:32:29 +0100

    v1.1.0: Calls to flush() will not destroy the console. v1.1.0: Updating dependency.

--- index.js
+++ index.js
@@ -5,49 +5,37 @@ module.exports.default = responseConsole;
 module.exports.responseConsole = responseConsole;
 module.exports.middleware = responseConsoleMiddleware;
 
-function responseConsole(res, console, bufferLimit = 8192, prefix = null) {
-  res.console = {
-    log() { console.log.apply(console, arguments) },
-    warn() { console.warn.apply(console, arguments) },
-    error() { console.error.apply(console, arguments) },
-    info() { console.info.apply(console, arguments) },
-    table() { console.table.apply(console, arguments) },
-    group() { console.group.apply(console, arguments) },
-    groupEnd() { console.groupEnd.call(console) },
-    buffer: undefined,
-    flush: undefined
-  };
+function responseConsole(res, console, bufferLimit = 10240, prefix = null) {
+  const buffer = new ConsoleBuffer(console, bufferLimit, prefix);
 
-  const buffer = res.console.buffer = new ConsoleBuffer(res.console, bufferLimit, prefix);
-
-  const endBuffering = function () {
+  const destroy = function () {
     buffer.flush();
 
     // All future writes will be flushed
-    buffer.limit = -1;
+    buffer._limit = -1;
 
     // Remove the process.exit listener from buffer
-    buffer.processExitListener && process.off('exit', buffer.processExitListener);
+    buffer._processExitListener && process.off('exit', buffer._processExitListener);
 
-    buffer.processExitListener = null;
+    buffer._processExitListener = null;
 
     // Disconnect the buffer from the response
     res.console = console;
-
-    this.buffer = null;
   };
 
-  res.console.flush = endBuffering;
+  res.console = buffer;
 
-  res.on('finish', endBuffering);
+  buffer.destroy = destroy;
+
+  res.on('finish', destroy);
 
   return res.console;
 }
 
-function responseConsoleMiddleware(console, bufferLimit = 8192, prefix = null) {
+function responseConsoleMiddleware(console, bufferLimit = 10240, prefix = null) {
   return function (req, res, next) {
     responseConsole(res, console, bufferLimit, prefix)
-      .log(req.method + " " + req.url + "\n" + JSON.stringify(req.body));
+      .log(req.method + " " + req.url + "\n" + JSON.stringify(req.body, null, 2));
 
     next();
   }
